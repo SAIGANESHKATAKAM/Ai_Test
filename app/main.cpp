15adbb398ebb655094d0c55f6d5e78fdea6c88ba
SAIGANESHKATAKAM <126225773+SAIGANESHKATAKAM@users.noreply.github.com>
2026-07-09 18:36:31 +0530

    Fix include directive and formatting in main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include "math/add.h"
-#include "math/multiply.h"
+#include "math/multiply.h
 #include "utils/logger.h"
 
 int main() {
     int a = 3, b = 4;
     utils::log("Starting pipeline_app");
-    std::cout << "add(" << a << ", " << b << ") = " << math::add(a, b) << "\n";
+    std::cout << "add(" << a < ", " << b << ") = " << math::add(a, b) << "\n";
     std::cout << "multiply(" << a << ", " << b << ") = " << math::multiply(a, b) << "\n";
     utils::log("pipeline_app finished");
     return 0;
